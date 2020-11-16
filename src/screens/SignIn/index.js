dd44c16262f86ee9dd5b498537b6bc427062b434
Matt Eric <mf9646a@alumni.american.edu>
2020-11-16 09:14:23 -0500

    Add sign up navigation

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -9,11 +9,18 @@ const styles = StyleSheet.create({
   },
 });
 
-const SignInScreen = ({ onSignIn }) => {
+const SignInScreen = ({ onSignIn, navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Public Sign In Screen</Text>
       <Button title="Sign In" onPress={onSignIn} />
+
+      <Text>OR</Text>
+
+      <Button
+        title="Go to Sign Up"
+        onPress={() => navigation.navigate('Sign Up')}
+      />
     </View>
   );
 };
