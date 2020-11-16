dbb11689d0ea8e4fef876197a0cd72aadc39c3e7
Matt Eric <mf9646a@alumni.american.edu>
2020-11-16 08:26:10 -0500

    Replace navigation destination with sign-in component

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -14,8 +14,8 @@ const LandingScreen = ({ navigation }) => {
     <View style={styles.container}>
       <Text>Public Landing Screen</Text>
       <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
+        title="Go to Sign In"
+        onPress={() => navigation.navigate('Sign In')}
       />
     </View>
   );
